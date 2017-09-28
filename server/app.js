4ed3134edb4ea035d583b9c0dccdbff3f785d2ba
Sanchay Shukla <sanchay.shukla@smartdatainc.net>
2017-09-28 11:36:49 +0530

    Working copy

---
+++ server/app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://riskpal.co.uk/demo-app");
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 4000;
+
+require("./routes.js")(app);
+app.listen(port);
+console.log("API Server running on port " + port);
